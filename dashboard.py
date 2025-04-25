0f0df459be1313d80ef17da636e8cdfcb34ad7b8
dashboard2026 <rafaelgomes0812@gmail.com>
2025-04-25 11:49:24 -0300

    Add files via upload

---
+++ dashboard.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(page_title="BASE DE DADOS AVANÇADA", layout="wide")
+
+# Título
+st.title("👟 ⚽ 🎒 👕 PRÉ-VENDA-SS26")
+
+# Caminho do arquivo Excel local
+caminho_arquivo = "CARTEIRA_DI_GASPI_24.04.xlsx"
+
+try:
+    df = pd.read_excel(caminho_arquivo, engine='openpyxl')
+
+    # Mostrar a tabela
+    st.subheader("📝 INFORMAÇÕES")
+    st.dataframe(df)
+except FileNotFoundError:
+    st.error("Arquivo não encontrado. Verifique o caminho.")
+except Exception as e:
+    st.error(f"Ocorreu um erro ao ler o arquivo: {e}")
